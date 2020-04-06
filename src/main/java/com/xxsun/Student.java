197c5c66422259c4e245a257d8b1cde03c423887
SunKenneth <xx.sun@msa.hinet.net>
2020-04-06 15:03:45 +0800

    java exec

---
+++ src/main/java/com/xxsun/Student.java
@@ -0,0 +1,16 @@
+package com.xxsun;
+
+public class Student {
+
+    String id;
+    String name;
+    int  engilsh;
+    int  math;
+
+    public Student(String id, String name, int engilsh, int math) {
+        this.id = id;
+        this.name = name;
+        this.engilsh = engilsh;
+        this.math = math;
+    }
+}
---
+++ src/main/java/com/xxsun/startStuent.java
@@ -0,0 +1,7 @@
+package com.xxsun;
+
+public class startStuent {
+    public static void main(String[] args) {
+
+    }
+}
